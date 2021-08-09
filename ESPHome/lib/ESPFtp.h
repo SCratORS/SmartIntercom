7c24eae735d5a039569b324320cfa57f805160de
SCratORS <scrators@mail.ru>
2021-08-09 20:58:15 +0300

    Change library

--- ESPHome/lib/ESPFtp.h
+++ ESPHome/lib/ESPFtp.h
@@ -1,26 +1,24 @@
 #include "esphome.h"
 #include "LittleFS.h"
-#include "ESPFtpServer.h"
+#include "FTPServer.h"
 
 #define DEBUGf(format, ...) ESP_LOGD("ftp", format "\n", ##__VA_ARGS__)
 
 class ESPFtp : public Component {
     private:
-
+        FTPServer ftpSrv = FTPServer(LittleFS);
     public:
-        FtpServer ftpSrv;
-        
-        void setup() override {
+        ESPFtp(const char* username, const char* password) {
             if (LittleFS.begin()) {
                 DEBUGf ("FTP Server start");
-                ftpSrv.begin ("esp", "esp");
+                ftpSrv.begin (username, password);
             } else {
                 DEBUGf ("File system could not be opened; ftp server will not work");
             }
         }
-        
+
         void loop() override {
-            ftpSrv.handleFTP (LittleFS);  
+            ftpSrv.handleFTP ();  
         }    
 };
 
