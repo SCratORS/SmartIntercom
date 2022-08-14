3a31d257ff9af4d6dfdaff503caa1e6035bb1e1f
SCratORS <scrators@mail.ru>
2022-08-14 13:19:33 +0300

    ESP8266 Only

--- ESPHome/lib/ESPFtp.h
+++ ESPHome/lib/ESPFtp.h
@@ -1,12 +1,5 @@
 #include "esphome.h"
-
-#ifdef ESP32
-    #include "FS.h"
-    #include "SPIFFS.h"
-#else
-    #include "LittleFS.h"
-#endif
-
+#include "LittleFS.h"
 #include "FTPServer.h"
 
 #define DEBUGf(format, ...) ESP_LOGD("ftp", format "\n", ##__VA_ARGS__)
@@ -16,13 +9,7 @@ class ESPFtp : public Component {
         FTPServer * ftpSrv;
     public:
         ESPFtp(const char* username, const char* password) : ftpSrv(NULL) {
-        
-        #ifdef ESP32
-            if (SPIFFS.begin()) this->ftpSrv = new FTPServer(SPIFFS);
-        #else
             if (LittleFS.begin()) this->ftpSrv = new FTPServer(LittleFS);
-        #endif
-        
             if (this->ftpSrv) this->ftpSrv->begin (username, password);
             else DEBUGf ("File system could not be opened; ftp server will not work");
         }
