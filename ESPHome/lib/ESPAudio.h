9c818204beb2937ee415f3b7603cf8399f7405ca
SCratORS <scrators@mail.ru>
2023-02-16 21:50:56 +0300

    Add play bytes data. Refactoring

--- ESPHome/lib/ESPAudio.h
+++ ESPHome/lib/ESPAudio.h
@@ -191,7 +191,7 @@ protected:
         dot++;
         if ((dot[0] == 'w' || dot[0] == 'w') &&
             (dot[1] == 'a' || dot[1] == 'A') &&
-            (dot[2] == 'v' || dot[2] == 'W') &&
+            (dot[2] == 'v' || dot[2] == 'V') &&
             (dot[3] == 0)) {
             return WAV;
         }
